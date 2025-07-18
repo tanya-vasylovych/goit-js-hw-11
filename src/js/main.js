92ddccf08629f1bbe404ce5331d5d838f81b380b
Tanya Vasylovych <tetiana.vasylovych.work@gmail.com>
2025-07-18 19:05:06 +0300

    gallery

---
+++ src/js/main.js
@@ -0,0 +1,36 @@
+// Описаний у документації
+import iziToast from "izitoast";
+// Додатковий імпорт стилів
+import "izitoast/dist/css/iziToast.min.css";
+
+import { getImagesByQuery } from './pixabay-api';
+import { createGallery, clearGallery, showLoader, hideLoader } from './render-functions';
+
+const form = document.querySelector('.form');
+
+const onFormSubmit = event => {
+    event.preventDefault();
+    clearGallery();
+    const query = event.currentTarget.elements['search-text'].value.trim();
+   
+    showLoader();
+
+  getImagesByQuery(query)
+    .then(data => {
+        if (data.hits.length === 0) {
+            iziToast.info({
+                message: `Sorry, there are no images matching your search query. Please try again!`,
+            });
+        } else {
+            createGallery(data.hits);
+        }
+    })
+    .catch(error => {
+        iziToast.error({ message: `Error: ${error.message}` });
+    })
+    .finally(() => {
+        hideLoader();
+    });
+};
+
+form.addEventListener('submit', onFormSubmit);
