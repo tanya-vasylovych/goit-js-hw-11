1a41606fc38f3fe5af3bad447d30ebf3dda090e4
Tanya Vasylovych <tetiana.vasylovych.work@gmail.com>
2025-07-18 20:45:15 +0300

    js

--- src/main.js
+++ src/main.js
@@ -3,8 +3,8 @@ import iziToast from "izitoast";
 // Додатковий імпорт стилів
 import "izitoast/dist/css/iziToast.min.css";
 
-import { getImagesByQuery } from './pixabay-api';
-import { createGallery, clearGallery, showLoader, hideLoader } from './render-functions';
+import { getImagesByQuery } from '../src/js/pixabay-api';
+import { createGallery, clearGallery, showLoader, hideLoader } from '../src/js/render-functions';
 
 const form = document.querySelector('.form');
 
